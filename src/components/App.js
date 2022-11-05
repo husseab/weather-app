154f5e7b5b4f232c41b94125f680d77938d4d400
abdikarim hussein <abdikarim.hussein557@gmail.com>
2022-11-05 15:51:56 +0000

    first component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,30 @@
 import { PropTypes } from "prop-types";
 import React from "react";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
-function App({ location }) {
-  const { city, country } = location;
+function App({ forecasts, location }) {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <LocationDetails city={city} country={country} />{" "}
+      <LocationDetails city={location.city} country={location.country} />
+      <ForecastSummaries forecasts={forecasts} />
     </div>
   );
 }
 
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
---
+++ src/components/ForecastSummaries.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ForecastSummary from "./ForecastSummary";
+
+function ForecastSummaries({ forecasts }) {
+  return (
+    <div className="forecast-summaries">
+      {forecasts.map((forecast) => (
+        <ForecastSummary
+          date={forecast.date}
+          description={forecast.description}
+          icon={forecast.icon}
+          temperature={forecast.temperature}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default ForecastSummaries;
+
+ForecastSummaries.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number.isRequired,
+      description: PropTypes.string.isRequired,
+      icon: PropTypes.string.isRequired,
+      temperature: PropTypes.shape({
+        min: PropTypes.number,
+        max: PropTypes.number,
+      }),
+    })
+  ).isRequired,
+};
