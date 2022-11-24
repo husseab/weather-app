b1485dd460ba9c5d37d6641f98b6f23d617ada36
abdikarim hussein <abdikarim.hussein557@gmail.com>
2022-11-24 13:07:19 +0000

    css

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -17,7 +17,11 @@ function ForecastSummary(props) {
         &deg;C
       </div>
       <div className="forecast-summary__description">{description}</div>
-      <button type="button" onClick={() => onSelect(date)}>
+      <button
+        className="search-button"
+        type="button"
+        onClick={() => onSelect(date)}
+      >
         More Details
       </button>
     </div>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,13 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "../styles/SearchForm.css";
 
 function SearchForm({ searchText, setSearchText, onSubmit }) {
   const handleInputChange = (event) => setSearchText(event.target.value);
 
   return (
     <div className="search-form">
-      <input type="text" onChange={handleInputChange} value={searchText} />
-      <button type="submit" onClick={onSubmit}>
+      <input
+        className="search-input"
+        type="text"
+        onChange={handleInputChange}
+        value={searchText}
+      />
+      <button className="search-button" type="submit" onClick={onSubmit}>
         Search
       </button>
     </div>
